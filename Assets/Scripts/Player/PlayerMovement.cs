c7566b3f94dd80525f898731e4b7c58601df694d
Michael Miles <boat081062@gmail.com>
2015-03-29 17:32:03 -0500

    Videos 1 and  2
    Completed, again... videos one and two. Added player and player
    controller.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,60 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    // Declaration of Public variables
+    public float speed = 6.0f;
+
+    // Declaration of Private variables
+    private Vector3 movement;
+    private Animator anim;
+    private Rigidbody playerRigidbody;
+    private int floorMask;
+    private float camRayLength = 100.0f;
+
+    // Awake is called when the script instance is being loaded
+    void Awake()
+    {
+        // Setting up references
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // This function is called every fixed framerate frame, if the MonoBehaviour is enabled
+    public void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+        Turning();
+        Animating(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0.0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+        if(Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0.0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+    void Animating(float h, float v)
+    {
+        bool walking = h != 0.0f || v != 0.0f;
+        anim.SetBool("IsWalking", walking);
+    }
 }
